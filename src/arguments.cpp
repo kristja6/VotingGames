491e925796ee738649662fe4cabc56a5d5532b68
Matyas Kristan <matyas.kristan@gmail.com>
2020-04-14 11:34:19 +0200

    removed dead code

--- src/arguments.cpp
+++ src/arguments.cpp
@@ -7,26 +7,6 @@
 
 bool Arguments::ReadArguments(int argc, const char **argv) {
   if (argc <= 1) return false;
-  /*for (int i = 1; i < argc; ++i) {
-    if (argv[i][0] == '-') { // a switch argument
-      size_t len = strlen(argv[i]);
-      for (int j = 1; j < (int)len; ++j) {
-        string res;
-        switch (argv[i][j]) { // display help
-          case 'z': isZeroOneGame = true; break;
-          case '-':
-            for (size_t k = j + 1; k < len; ++ k) res.push_back(argv[i][k]);
-            namedArguments.insert(res);
-            j = len;
-            break;
-          default:
-            return false;
-        }
-      }
-    } else { // is the optimized function
-      inputFileName = string(argv[i]);
-    }
-  }*/
   string allArgs;
   for (int i = 1; i < argc; ++ i) {
     allArgs += string(argv[i]) + " ";
